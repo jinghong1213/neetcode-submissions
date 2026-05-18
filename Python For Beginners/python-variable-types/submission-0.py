f6a0c2a58efad715ed59770191ecedd9d813809c
Yap Jing Hong <45033500+jinghong1213@users.noreply.github.com>
2026-05-18 17:14:23 +0800

    Add: python-variable-types - submission-0

---
+++ Python For Beginners/python-variable-types/submission-0.py
@@ -0,0 +1,12 @@
+
+integer_type = 10
+float_type = 10.1
+bool_type = True
+string_type = "done"
+list_type = [2,3,4]
+
+print(type(integer_type))
+print(type(float_type))
+print(type(bool_type))
+print(type(string_type))
+print(type(list_type))
