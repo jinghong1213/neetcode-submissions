721bb6b7d6b5a38ec17b0865d91b2a8a34c17d66
Yap Jing Hong <45033500+jinghong1213@users.noreply.github.com>
2026-05-18 17:24:17 +0800

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,14 @@
+variable = 1213
+print(type(variable))
+
+variable = 1213.1999
+print(type(variable))
+
+variable = True
+print(type(variable))
+
+variable = "Jing"
+print(type(variable))
+
+variable = [1,2,3]
+print(type(variable))
