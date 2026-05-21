8f709ac5f022cd68b4b64e24b638be0a95c75120
Yap Jing Hong <45033500+jinghong1213@users.noreply.github.com>
2026-05-21 17:33:02 +0800

    Add: python-length-function - submission-1

---
+++ Python For Beginners/python-length-function/submission-1.py
@@ -0,0 +1,20 @@
+def get_longer_word(word1: str, word2: str) -> str:
+    str1 = len(word1)
+    str2 = len(word2)
+    if str1 > str2:
+        return word1
+    elif str2 > str1:
+        return word2
+    else:
+        return word1
+
+    #faster Solution
+    #if len(word1) >= len(word2):
+    #    return word1
+    #return word2
+
+
+# do not modify below this line
+print(get_longer_word("yellow", "orange"))
+print(get_longer_word("red", "blue"))
+print(get_longer_word("green", "blue"))
